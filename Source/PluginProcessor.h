65390f9b1da036f0fb62072bc5026dc5f091806f
Wade Andersen <82845861+wadeAndersen@users.noreply.github.com>
2022-03-06 11:45:44 -0600

    added sliders to the GUI

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -71,8 +71,7 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
-    
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();    
     juce::AudioProcessorValueTreeState apvts {*this, nullptr, "Parameters", createParameterLayout()};
     
 private:
